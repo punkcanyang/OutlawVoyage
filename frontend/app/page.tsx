2685ab69c4c4de8e6a6f962925dec3579fc4c511
0xdiymax <jarvis3x@gmail.com>
2024-08-06 22:18:08 +0800

    feat: 加入table

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,97 +1,83 @@
+"use client"
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@/components/ui/select";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { EspoirABI } from '../abi/Espoir'
+import { useReadContract, useAccount } from "wagmi";
+import { cn } from "@/lib/utils"
+
+const contractAddress = '0x8539989C5fFce3660937a7d00EC852421428E4E9'
 
 export default function Home() {
+  const { address } = useAccount()
+
+  const { data: shipData, isLoading } = useReadContract({
+    abi: EspoirABI,
+    address: contractAddress,
+    functionName: 'getShip',
+    args: [BigInt(1)],
+  })
+
+  const { data: voyageId } = useReadContract({
+    abi: EspoirABI,
+    address: contractAddress,
+    functionName: 'getNextVoyageId',
+    args: [BigInt(1)],
+  })
+
+  console.log({
+    shipData,
+    voyageId,
+  });
+
   return (
-    <main className="container px-4 max-w-4xl flex min-h-screen flex-col py-12 gap-4">
+    <main className="container px-4 max-w-4xl flex min-h-screen flex-col py-12 gap-4 bg-[length:100%_100%] bg-no-repeat" style={{ backgroundImage: `url('/images/johnwick.jpg')` }}>
       <nav className="flex justify-end mb-12">
         <ConnectButton />
       </nav>
 
       <div>
-        <div className="flex flex-col items-center">
-          <h1 className="text-3xl font-bold mb-4">石头剪刀布</h1>
-          <h2 className="text-xl mb-4">赌博显示录</h2>
-          <h2 className="text-3xl">Web 3</h2>
-        </div>
-
-        <div className="flex items-center mb-4 mt-6">
-          <span className="font-semibold">cruise detail</span>
-        </div>
-
-        <div className="grid grid-cols-2 gap-4 mb-4">
-          <div>
-            <label className="block text-sm font-medium mb-1">入场金</label>
-            <Select>
-              <SelectTrigger>
-                <SelectValue placeholder="选择金额"/>
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="1u">1u</SelectItem>
-                <SelectItem value="10u">10u</SelectItem>
-                <SelectItem value="100u">100u</SelectItem>
-                <SelectItem value="1000u">1000u</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-          <div>
-            <label className="block text-sm font-medium mb-1">游戏时长</label>
-            <Select>
-              <SelectTrigger>
-                <SelectValue placeholder="选择时长"/>
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="30m">30m</SelectItem>
-                <SelectItem value="1h">1h</SelectItem>
-                <SelectItem value="2h">2h</SelectItem>
-                <SelectItem value="4h">4h</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-        </div>
-
-        <div className="flex space-x-4 mb-4">
-          <Button variant="outline">开始时间</Button>
-          <span className="flex items-center">today tmr</span>
-        </div>
+        <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20 mb-12">
+          Outlaw Voyage
+        </h1>
 
         <div className="mb-4">
-          <h3 className="text-lg font-semibold mb-2">当前游戏列表</h3>
-          <div className="flex justify-end mb-2">
-            <Select>
-              <SelectTrigger className="w-24">
-                <SelectValue placeholder="Filter"/>
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="asc">Ascending</SelectItem>
-                <SelectItem value="desc">Descending</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
+          {/*<h3 className="text-lg font-semibold mb-2 text-white">游轮列表</h3>*/}
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {[1, 2, 3].map((item, index) => (
               <Link href={`/cruise/${index + 1}`} key={index} className="block transition-transform hover:scale-105">
-                <Card className="h-full cursor-pointer hover:shadow-lg">
-                  <CardHeader>
-                    <CardTitle>Sample Game {index + 1}</CardTitle>
-                  </CardHeader>
-                  <CardContent>
-                    <p><strong>开始时间:</strong> 2024-07-27 10:00</p>
-                    <p><strong>结束时间:</strong> 2024-07-27 12:00</p>
-                    <p><strong>人数上限:</strong> 10</p>
-                    <p><strong>入场金:</strong> 100u</p>
-                    <p><strong>总奖金:</strong> 1000u</p>
-                  </CardContent>
-                </Card>
+                <CardDemo />
               </Link>
             ))}
           </div>
         </div>
       </div>
     </main>
+  );
+}
+
+function CardDemo() {
+  return (
+    <div className="max-w-xs w-full group/card">
+      <div
+        className={cn(
+          "cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl  max-w-sm mx-auto backgroundImage flex flex-col p-4",
+          "bg-[url('/images/home-card.jpg')] bg-cover"
+        )}
+      >
+        <div className="absolute w-full h-full top-0 left-0 transition duration-300 group-hover/card:bg-black opacity-60"></div>
+        <h1 className="font-bold text-xl md:text-2xl text-gray-50 relative z-10 mb-8">
+          Sample Game
+        </h1>
+
+        <div className="text content font-normal text-gray-50 space-y-1.5">
+          <p><strong>开始时间:</strong> 2024-07-27 10:00</p>
+          <p><strong>结束时间:</strong> 2024-07-27 12:00</p>
+          <p><strong>人数上限:</strong> 10</p>
+          <p><strong>入场金:</strong> 100u</p>
+          <p><strong>总奖金:</strong> 1000u</p>
+        </div>
+      </div>
+    </div>
   );
 }
